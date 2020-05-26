0565e0daed04b2e94e3013def211923883ef893e
Codey Du <codeydu@hotmail.com>
2020-05-26 22:11:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Buttons from './components/Buttons.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const numRegex = /[0-9]/
+const signRegex = /[*+/-]/
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: [],
+      results: []
+    }
+    //Bind methods here
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleEval  = this.handleEval.bind(this);
+    this.handleNumber = this.handleNumber.bind(this);
+  }
+  handleClear() {
+    this.setState({
+      message: [],
+      results: []
+    })
+  }
+  handleClick(event) {
+    if (event.target.value === "backSpace") {
+      this.setState({
+          message: this.state.message.slice(0, -1)
+      })
+    }
+    else if (this.state.message.includes(".") && event.target.value === ".") {
+      //Do nothing when decimal is already present and next keyClick is decimal again
+    }
+    else if (!this.state.message.includes("=")){
+      this.setState({
+          message: this.state.message.concat(event.target.value)
+        })
+    }
+    else if (this.state.message.includes("=") && !numRegex.test(event.target.value)){
+      this.setState({
+          message: this.state.results.slice(-1).concat(event.target.value),
+          results: []
+      })
+    }
+    else {
+      this.setState({
+          message: [].concat(event.target.value),
+          results: []
+      })
+    }
+  }
+  handleNumber(event) {
+    let number = Number(event.target.value)
+    this.setState({
+      message: this.state.message.concat(number)
+    })
+  }
+  handleEval() {
+    if(this.state.message.includes('=')){
+      alert('Do not press = without entering number or +-*/')
+    }
+    else if(numRegex.test(this.state.message.slice(-1))){
+      let sum = eval(this.state.message.join(""))
+      
+      this.setState({
+        message: this.state.message.concat('=').concat(sum),
+        results: this.state.results.concat(sum)
+      })
+      console.log(this.state.message)
+    }
+    else{
+      alert('incorrect input, will not evaluate!')
+    }
+    
+  }
+  render() {
+    console.log(this.state.message, this.state.results)
+    return (
+      <div className="calculator">
+        <h1>{this.state.message}</h1>
+        <h1>{this.state.results[this.state.results.length-1]}</h1>
+        <Buttons 
+          handleNumber={this.handleNumber}
+          handleClick={this.handleClick} 
+          handleClear={this.handleClear}
+          handleEval ={this.handleEval}/>
+        <p className='bottomCaption'>Coded by Codey Du! 2019 November 21st</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Calculator;
---
+++ src/components/Buttons.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Buttons extends React.Component {
+    render() {
+        return(
+        <div className='buttonSection'>
+            <button className="wideButton" onClick={this.props.handleClear}>AC</button>
+            <button value="/" onClick={this.props.handleClick}>/</button>
+            <button value="*" onClick={this.props.handleClick}>x</button>
+            <button value="7" onClick={this.props.handleNumber}>7</button>
+            <button value="8" onClick={this.props.handleNumber}>8</button>
+            <button value="9" onClick={this.props.handleNumber}>9</button>
+            <button value="-" onClick={this.props.handleClick}>-</button>
+            <button value="4" onClick={this.props.handleNumber}>4</button>
+            <button value="5" onClick={this.props.handleNumber}>5</button>
+            <button value="6" onClick={this.props.handleNumber}>6</button>
+            <button value="+" onClick={this.props.handleClick}>+</button>
+            <button value="1" onClick={this.props.handleNumber}>1</button>
+            <button value="2" onClick={this.props.handleNumber}>2</button>
+            <button value="3" onClick={this.props.handleNumber}>3</button>
+            <button value="backSpace" onClick={this.props.handleClick}>←Back</button>
+            <button className="wideButton" value="0" onClick={this.props.handleClick}>0</button>
+            <button value="." onClick={this.props.handleClick}>.</button>
+            <button value="=" onClick={this.props.handleEval}>=</button>
+        </div>
+        )
+    }
+}
+
+export default Buttons
