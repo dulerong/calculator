6ed7cdbafef7eebe42cc86eac131894cde977a68
Codey Du <codeydu@hotmail.com>
2020-05-26 22:12:32 +0900

    Updates

---
+++ precache-manifest.e4ebd42d6e25036bb4cb427817269aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0184902c9f940d16dcbad9a065a39b41",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "af2980a49880aecb1ed9",
+    "url": "/calculator/static/css/main.e3ee6a07.chunk.css"
+  },
+  {
+    "revision": "3dd38cfdd90c948a3d36",
+    "url": "/calculator/static/js/2.b28a8962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b28a8962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2980a49880aecb1ed9",
+    "url": "/calculator/static/js/main.15096297.chunk.js"
+  },
+  {
+    "revision": "53122c127a7fe401835e",
+    "url": "/calculator/static/js/runtime-main.6de0c573.js"
+  }
+]);
